fd549f4724b829bd0570f681897f0ebe64aa0fd8
Andy Fillebrown <andy.fillebrown@gmail.com>
2022-09-03 09:33:14 -0400

    Cleanup

--- src/playground.js
+++ src/playground.js
@@ -1,6 +1,6 @@
 var createScene = function () {
     const scene = new BABYLON.Scene(engine)
-    scene.enablePhysics(new BABYLON.Vector3(0, -4, 0), new BABYLON.OimoJSPlugin(false, 20, OIMO))
+    scene.enablePhysics(new BABYLON.Vector3(0, -4, 0), new BABYLON.OimoJSPlugin(true, 1, OIMO))
     const physicsEngine = scene.getPhysicsEngine();
     physicsEngine.setSubTimeStep(10);
 
@@ -11,20 +11,10 @@ var createScene = function () {
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene)
     light.intensity = 0.7
 
-
-    const sphereCount = 10
-    // const spheres = []
-    // for (let i = 0; i < sphereCount; i++) {
-    //     let sphere = BABYLON.MeshBuilder.CreateSphere(`sphere`, { diameter: 1, segments: 32 }, scene)
-    //     sphere.position.set(-10, 12, 0)
-    //     sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.95,  }, scene)
-    //     sphereImposters.push(sphere.physicsImpostor)
-    // }
-
     const plane1 = BABYLON.MeshBuilder.CreatePlane(`plane 1`, { size: 8, sideOrientation: BABYLON.Mesh.DOUBLESIDE })
     plane1.rotateAround(BABYLON.Vector3.ZeroReadOnly, BABYLON.Vector3.RightReadOnly, Math.PI / 2)
     plane1.rotateAround(BABYLON.Vector3.ZeroReadOnly, BABYLON.Vector3.RightHandedForwardReadOnly, Math.PI / 8)
-    // plane1.scaling.y = 0.25
+    plane1.scaling.y = 0.25
     plane1.position.set(-7, 0, 0)
     plane1.physicsImpostor =  new BABYLON.PhysicsImpostor(plane1, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1 }, scene)
     console.log(plane1.physicsImpostor.physicsBody)
@@ -32,34 +22,21 @@ var createScene = function () {
 
     const plane2 = plane1.clone()
     plane2.rotateAround(BABYLON.Vector3.ZeroReadOnly, BABYLON.Vector3.RightHandedForwardReadOnly, -Math.PI / 3)
-    // plane2.scaling.x = 2
+    plane2.scaling.x = 2
     plane2.position.set(7, 0, 0)
     plane2.physicsImpostor =  new BABYLON.PhysicsImpostor(plane2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1 }, scene)
     plane2.physicsImpostor.physicsBody.shapes.collidesWith = 2
-
-    // plane1.physicsImpostor.registerOnPhysicsCollide(sphereImposters, (collider, collidedAgainst, point) => {
-    //     console.debug(`Sphere collided with plane 1`)
-    // })
-
-    // plane2.physicsImpostor.registerOnPhysicsCollide(sphereImposters, (collider, collidedAgainst, point) => {
-    //     console.debug(`Sphere collided with plane 2`)
-    // })
 
     const onCollide = (collider, collidedAgainst, point) => {
         // console.debug(`onCollide`)
     }
 
-
-
     setInterval(() => {
         let sphere = BABYLON.MeshBuilder.CreateSphere(`sphere`, { diameter: 0.5, segments: 32 }, scene)
         sphere.position.set(-10, 12, 0)
         sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.95,  }, scene)
-        // console.log(sphere.physicsImpostor.physicsBody)
         sphere.physicsImpostor.physicsBody.shapes.belongsTo = 2
         sphere.physicsImpostor.physicsBody.shapes.collidesWith = 1
-        // console.log(sphere.physicsImpostor.physicsBody)
-        //sphere.physicsImpostor.physicsBody.collisionFilterMask = [ plane1.physicsImpostor.physicsBody, plane2.physicsImpostor.physicsBody ]
         plane1.physicsImpostor.registerOnPhysicsCollide(sphere.physicsImpostor, onCollide)
         plane2.physicsImpostor.registerOnPhysicsCollide(sphere.physicsImpostor, onCollide)
     }, 100)
