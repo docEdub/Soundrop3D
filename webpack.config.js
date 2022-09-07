535184e4140a66380e2dc7036ec7c6a9aecfc080
Andy Fillebrown <afillebrown@microsoft.com>
2022-09-07 15:39:27 -0400

    Switch ammo.js package to github:kripken/ammo.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path')
 
 const externals = [
-    {
-        name: 'ammo.js',
-        globalVarName: 'Ammo',
-        src: 'https://cdn.babylonjs.com/ammo.js',
-    },
+    // {
+    //     name: 'ammo.js',
+    //     globalVarName: 'Ammo',
+    //     src: 'https://raw.githubusercontent.com/kripken/ammo.js/main/builds/ammo.js',
+    // },
     {
         name: 'babylonjs',
         globalVarName: 'BABYLON',
@@ -42,5 +42,11 @@ module.exports = {
             template: path.resolve(__dirname, 'src', 'index.html'),
             title: 'soundrop3d',
         }),
-    ]
+    ],
+    resolve: {
+        fallback: {
+            'fs': false,
+            'path': false, // ammo.js seems to also use path
+        }
+    }
 }
