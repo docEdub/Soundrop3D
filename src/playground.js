177fbc6fc3ebef3b4a20be04fcb3e01bb7c58074
Andy Fillebrown <andy.fillebrown@gmail.com>
2022-09-16 10:51:52 -0400

    Add class `Ball`

--- src/playground.js
+++ src/playground.js
@@ -1,7 +1,6 @@
 var createScene = function () {
-    const ballPoolCount = 200
-    const ballStartPosition = new BABYLON.Vector3(-10, 12, 0)
-    const ballDropsPerMinute = 120
+    const BallPoolCount = 200
+    const BallDropsPerMinute = 120
 
     const scene = new BABYLON.Scene(engine)
     scene.enablePhysics(new BABYLON.Vector3(0, -4, 0), new BABYLON.AmmoJSPlugin(false, ammo))
@@ -12,63 +11,112 @@ var createScene = function () {
     const light = new BABYLON.HemisphericLight(`light`, new BABYLON.Vector3(0, 1, 0), scene)
     light.intensity = 0.7
 
-    const plane1 = BABYLON.MeshBuilder.CreatePlane(`plane 1`, { size: 8, sideOrientation: BABYLON.Mesh.DOUBLESIDE })
+    const plane1 = BABYLON.MeshBuilder.CreateBox(`plane 1`, { size: 8 })
     plane1.rotateAround(BABYLON.Vector3.ZeroReadOnly, BABYLON.Vector3.RightReadOnly, Math.PI / 2)
     plane1.rotateAround(BABYLON.Vector3.ZeroReadOnly, BABYLON.Vector3.RightHandedForwardReadOnly, Math.PI / 8)
     plane1.scaling.y = 0.25
+    plane1.scaling.z = 0.01
     plane1.position.set(-7, 0, 0)
-    plane1.physicsImpostor =  new BABYLON.PhysicsImpostor(plane1, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1 }, scene)
+    plane1.physicsImpostor = new BABYLON.PhysicsImpostor(plane1, BABYLON.PhysicsImpostor.PlaneImpostor, { mass: 0, restitution: 1 }, scene)
 
-    const plane2 = plane1.clone(`plane 2`)
+    const plane2 = plane1.clone(`plane 2`, null, false, false)
     plane2.rotateAround(BABYLON.Vector3.ZeroReadOnly, BABYLON.Vector3.RightHandedForwardReadOnly, -Math.PI / 3)
     plane2.scaling.x = 2
     plane2.position.set(7, 0, 0)
-    plane2.physicsImpostor =  new BABYLON.PhysicsImpostor(plane2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1 }, scene)
+    plane2.physicsImpostor = new BABYLON.PhysicsImpostor(plane2, BABYLON.PhysicsImpostor.PlaneImpostor, { mass: 0, restitution: 1 }, scene)
 
-    const onBallCollide = (ballCollider, planeCollider, point) => {
-        const ball = ballCollider.object
-        const now = Date.now()
-        if (200 < now - ball.lastCollisionTime) {
-            const playbackRate = 8 * (1 / planeCollider.object.scaling.x)
-            ball.tone.setPlaybackRate(playbackRate)
-            ball.tone.play()
-            ball.lastCollisionTime = now
+    //#region class Ball
+
+    class Ball {
+        static StartPosition = new BABYLON.Vector3(-10, 12, 0)
+
+        constructor(tone) {
+            this._.tone = tone
+
+            const mesh = BABYLON.MeshBuilder.CreateSphere(`ball`, { diameter: 0.5, segments: 32 }, scene)
+            mesh.position.set(0, -1000, 0)
+            mesh.physicsImpostor = new BABYLON.PhysicsImpostor(mesh, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.95 }, scene)
+            mesh.physicsImpostor.executeNativeFunction((world, body) => {
+                world.removeCollisionObject(body)
+                world.addRigidBody(body, 1, 2)
+            })
+            this._.mesh = mesh
+        }
+
+        addCollider = (physicsImposter) => {
+            this._.addCollider(physicsImposter)
+        }
+
+        removeCollider = (physicsImposter) => {
+            this._.removeCollider(physicsImposter)
+        }
+
+        drop = () => {
+            this._.drop()
+        }
+
+        _ = new class {
+            lastCollisionTime = 0
+            mesh = null
+            tone = null
+
+            get physicsImposter() {
+                return this.mesh.physicsImpostor
+            }
+
+            drop = () => {
+                const physicsImposter = this.physicsImposter
+                physicsImposter.setAngularVelocity(BABYLON.Vector3.ZeroReadOnly)
+                physicsImposter.setLinearVelocity(BABYLON.Vector3.ZeroReadOnly)
+
+                const mesh = this.mesh
+                mesh.position.copyFrom(Ball.StartPosition)
+            }
+
+            addCollider = (physicsImposter) => {
+                this.physicsImposter.registerOnPhysicsCollide(physicsImposter, this.onCollide)
+            }
+
+            removeCollider = (physicsImposter) => {
+                this.physicsImposter.unregisterOnPhysicsCollide(physicsImposter, this.onCollide)
+            }
+
+            onCollide = (ballCollider, planeCollider) => {
+                const planeMesh = planeCollider.object
+                if (!planeMesh) {
+                    return
+                }
+
+                const now = Date.now()
+                if (200 < now - this.lastCollisionTime) {
+                    this.lastCollisionTime = now
+
+                    const tone = this.tone
+                    tone.setPlaybackRate(8 * (1 / planeMesh.scaling.x))
+                    tone.play()
+                }
+            }
         }
     }
 
-    const ballPool = new Array(ballPoolCount)
+    //#endregion
+
+    const ballPool = new Array(BallPoolCount)
     let ballsReady = false
 
     BABYLON.Engine.audioEngine.lock()
     BABYLON.Engine.audioEngine.onAudioUnlockedObservable.addOnce(() => {
         const tone = new BABYLON.Sound(`tone`, `tone.wav`, scene, () => {
-            for (let i = 0; i < ballPoolCount; i++) {
-                const ball = BABYLON.MeshBuilder.CreateSphere(`ball`, { diameter: 0.5, segments: 32 }, scene)
-                ball.position.set(0, -1000, 0)
-
-                ball.physicsImpostor = new BABYLON.PhysicsImpostor(ball, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene)
-                ball.physicsImpostor.executeNativeFunction((world, body) => {
-                    world.removeCollisionObject(body)
-                    world.addRigidBody(body, 1, 2)
-                })
-                ball.physicsImpostor.registerOnPhysicsCollide(plane1.physicsImpostor, onBallCollide)
-                ball.physicsImpostor.registerOnPhysicsCollide(plane2.physicsImpostor, onBallCollide)
-                ball.lastCollisionTime = 0
-
-                ball.tone = tone.clone(``)
-
+            for (let i = 0; i < BallPoolCount; i++) {
+                const ball = new Ball(tone.clone(``))
+                ball.addCollider(plane1.physicsImpostor)
+                ball.addCollider(plane2.physicsImpostor)
                 ballPool[i] = ball
             }
-
-            const redMaterial = new BABYLON.StandardMaterial(`red`)
-            redMaterial.diffuseColor.set(1, 0.5, 0.5)
-            redMaterial.specularColor.set(1, 0.5, 0.5)
-            ballPool[0].material = redMaterial
 
             ballsReady = true
         })
     })
-
 
     let ballPoolIndex = 0
 
@@ -79,14 +127,11 @@ var createScene = function () {
 
         // console.debug(`dropping ball index ${ballPoolIndex}`)
         const ball = ballPool[ballPoolIndex]
-        ball.physicsImpostor.setAngularVelocity(BABYLON.Vector3.ZeroReadOnly)
-        ball.physicsImpostor.setLinearVelocity(BABYLON.Vector3.ZeroReadOnly)
-        ball.position.copyFrom(ballStartPosition)
-
-        ballPoolIndex = (ballPoolIndex + 1) % ballPoolCount
+        ball.drop()
+        ballPoolIndex = (ballPoolIndex + 1) % BallPoolCount
     }
 
-    let ballDropTimePeriodInMs = 1000 * (60 / ballDropsPerMinute)
+    let ballDropTimePeriodInMs = 1000 * (60 / BallDropsPerMinute)
     let timeFromLastBallDropInMs = 0
 
     scene.registerBeforeRender(() => {
